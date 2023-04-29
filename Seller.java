fb14502a4a7bd2a5573538e442a27f0bc0f526c8
barnet76 <129809416+barnet76@users.noreply.github.com>
2023-04-28 22:38:04 -0400

    added comments

--- Seller.java
+++ Seller.java
@@ -2,7 +2,17 @@ package pj5;
 
 import java.io.File;
 import java.util.ArrayList;
-
+/**
+ * Seller class
+ * <p>
+ * The Seller class represents a seller object which is a type of user that interacts with
+ * users of its type and customers. An intance of the class is created with attributes name,
+ * email and password.
+ *
+ * @author Amelia Williams, Meha Kavoori, Anish Puri, Tyler Barnett
+ * @version 4/28/2023
+ */
+// class extends user
 public class Seller extends User {
     private Customer[] customerList;
 
@@ -10,12 +20,13 @@ public class Seller extends User {
 
     private static final long serialVersionUID = 6387730094462016452L;
 
-
+    // the constructor uses super for some of its fields because it extends User
+    // User already contains some of these fields
     public Seller(String name, String email, String password) {
         super(name, email, password);
         listOfStores = new ArrayList<Store>();
     }
-
+    // following method allows for a seller to add a store using a String of the store name
     public void addStore(String storeName) throws IllegalArgumentException {
         ArrayList<Store> allStores = Store.getAllStores();
 
@@ -28,15 +39,17 @@ public class Seller extends User {
         listOfStores.add(st);
     }
 
-
+    // following method sets the customer list as a list
     public void setCustomerList(Customer[] customerList) {
         this.customerList = customerList;
     }
-
+    // following method gets an array list of all the stores that exist
     public ArrayList<Store> getListOfStores() {
         return listOfStores;
     }
-
+    // following method adds a customer by creating an array list that sets the customer list
+    // into each element of the array list. A new customer is added to the arraylist then the entire
+    // array list of customers is returned with the next for loop
     public void addCustomer(Customer c) {
         ArrayList<Customer> arr = new ArrayList<>();
         for (int i = 0; i < customerList.length; i++) {
@@ -50,46 +63,16 @@ public class Seller extends User {
         }
     }
 
-/*
-    public Messenger2 createConversation(Customer c1, Store st){
-        Messenger2 c = new Messenger2(c1, this, st);
-        return c;
-    }
-*/
 
+    // following method gets the customer at a certain index from a file named "accounts.ser"
     public Customer getCustomer(int index){
         File f = new File("accounts.ser");
         int count = 1;
-/*
-        for(User u: this.listUsers(f, this.getNumCreated())){
-            if(u instanceof Customer) {
-                if(!this.getBlockedUsers().contains(u)) {
-                    if(!this.getBlockedBy().contains(u)) {
-                        if(!u.getInvisibleTo().contains(this)){
-                            if (count == index) {
-                                return (Customer) u;
-                            }
-                            count++;
-                        }
-                    }
-                }
-            }
-        }
-*/
+
         return null;
     }
 
-    /*public String searchCustomer(int customerID) {
-        String customerEmail = "Customer Not Found";
-        for (int i = 0; i < customerList.length; i++) {
-            if (customerList[i].getCustomerID() == (customerID)) {
-                customerEmail = customerList[i].getEmail();
-            }
-        }
-        return customerEmail;
-    }
-
-     */
+   
     public void displayCustomers(){
         File f = new File("accounts.ser");
         int count = 1;
@@ -107,7 +90,10 @@ public class Seller extends User {
             }
         }
     }
-
+    @Override
+    // following method checks if an object is an instance of a seller before casting to a seller object to check
+    // if the seller's email and password equals that of a seller that currently exists
+    // and returns false if it is not equal
     public boolean equals(Object o){
         if(o instanceof Seller) {
             Seller seller = (Seller) o;
@@ -117,7 +103,7 @@ public class Seller extends User {
         }
         return false;
     }
-
+    // checks if user can message given object
     public boolean canMessage(Object o) {
         if (o instanceof Seller || o instanceof Customer) {
             return true;
@@ -126,9 +112,7 @@ public class Seller extends User {
     }
 
 
-    // add equals
-//        public Message newMessage() //in progress
-//        public Message newMessage() //in progress
+   
 
 
     }
