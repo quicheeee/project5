adaf332a349e285f57b0ed714dbee244df50e87a
barnet76 <129809416+barnet76@users.noreply.github.com>
2023-04-28 22:19:18 -0400

    Added Comments

--- Customer.java
+++ Customer.java
@@ -3,19 +3,28 @@ package pj5;
 import java.io.File;
 import java.io.Serializable;
 import java.util.ArrayList;
+/**
+ * Customer Class
+ * The customer class represents a Customer object which extends User.
+ * The customer has a list of stores in addition to all User methods.
+ *
+ * @author Amelia Williams, Meha Kavoori, Anish Puri, Tyler Barnett
+ * @version 4/28/2023
+ */
 
+// class implements serializable
 public class Customer extends User implements Serializable {
     private ArrayList<Store> storeList;
-
+    // Customer Class Constructor takes in values extended from User class.
     public Customer(String name, String email, String password) {
         super(name, email, password);
 
     }
-
+    // returns store list
     public ArrayList<Store> getStoreList() {
         return storeList;
     }
-
+    // sets store list
     public void setStoreList(ArrayList<Store> storeList) {
         this.storeList = storeList;
     }
@@ -35,6 +44,7 @@ public class Customer extends User implements Serializable {
     }
     
      */
+    // adds store to list
     public void addStore(Store s) {
         storeList.add(s);
     }
@@ -49,6 +59,7 @@ public class Customer extends User implements Serializable {
         return sellerEmail;
     }
      */
+    // returns desired seller from list of users
     public Seller getSeller(int index) {
         File f = new File("accounts.ser");
         int count = 1;
@@ -66,7 +77,7 @@ public class Customer extends User implements Serializable {
         }
         return null;
     }
-
+    // displays all sellers found in getUsers()
     public void displaySellers() {
         File f = new File("accounts.ser");
         int count = 1;
@@ -86,7 +97,7 @@ public class Customer extends User implements Serializable {
         }
     }
 
-    // fix equals
+    
 
     @Override // checks for customer objects with equal values
     public boolean equals(Object o) {
@@ -98,14 +109,15 @@ public class Customer extends User implements Serializable {
         }
         return false;
     }
-
+    
+    // checks if user can message other user
     public boolean canMessage(Object o) {
         if (o instanceof Seller) {
             return true;
         }
         return false;
     }
-    //public Message newMessage (); // in progress
+    
 
 
 }
