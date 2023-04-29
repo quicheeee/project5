152350d5b5cb7beed5ee47f3f889d3a089b92c36
barnet76 <129809416+barnet76@users.noreply.github.com>
2023-04-28 22:23:36 -0400

    added comments

--- Message.java
+++ Message.java
@@ -3,7 +3,15 @@ package pj5;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-
+/**
+ * Class Message
+ * The message class represents individual messages sent between different users.
+ * It takes String objects and converts the text to Message objects which can be stored.
+ *
+ * @author Amelia Williams, Meha Kavoori, Anish Puri, Tyler Barnett
+ * @version 04/10/2023
+ */
+// following class implements Serializabe and Comparable<Message> so that the messages can be compared
 public class Message implements Serializable {
     private User sender;
     private User receiver;
@@ -14,7 +22,7 @@ public class Message implements Serializable {
     private User deletedFor = null;
 
     private static final long serialVersionUID = 2L;
-
+    // following constructor passes all the previously stated fields along with a User message owner
     public Message(User sender, User receiver, String message, boolean disappearing) {
         this.sender = sender;
         this.receiver = receiver;
@@ -26,6 +34,11 @@ public class Message implements Serializable {
     }
 
     @Override
+    // following method checks if an object is equal to this and returns true. If not and o is null, it returns false.
+    // if true, a the object is cast as a message to check if sender equals the message sender,
+    // the receiver equals the message receiver
+    //, the owner equals  the message owner, and if the timestamp equals the message timestamp.
+    // if all of this is true, the message is returned
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
@@ -50,34 +63,35 @@ public class Message implements Serializable {
         return getMessage().equals(message1.getMessage());
     }
 */
+    // gets the sender field
     public User getSender() {
         return sender;
     }
-
+    // gets the receiver field
     public User getReceiver() {
         return receiver;
     }
-
+    // sets the Message field
     public void setMessage(String message) {
         this.message = message;
     }
-
+    // gets the message field
     public String getMessage() {
         return message;
     }
-
+    // gets the timestamp of when the message was created
     public String getCreateDate() {
         return this.createDate;
     }
-
+    // possible method for disappearing messages
     public boolean isDisappearing() {
         return disappearing;
     }
-
+    // returns user that the message is deleted for
     public User getDeletedFor() {
         return deletedFor;
     }
-
+    // sets user the message is deleted for
     public void setDeletedFor(User deletedFor) {
         this.deletedFor = deletedFor;
     }
